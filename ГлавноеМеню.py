68d913354d06f1a887e8d9db7fd571e2a2e76033
1 <vasvirina@edu.hse.ru>
2025-05-09 20:34:17 +0300

    Мой первый коммит

---
+++ ГлавноеМеню.py
@@ -0,0 +1,75 @@
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+# создаем сами кнопки
+START_KEYBOARD = ReplyKeyboardMarkup(
+    [
+        ["Зарядка", "Трекер сна"],
+        ["Трекер настроения", "Водный баланс"],
+        ["Трекер приема таблеток"]
+    ],
+    resize_keyboard=True
+)
+
+# подменю (тут пока только кнопка назад)
+CHARGE_KEYBOARD = ReplyKeyboardMarkup([["Назад"]], resize_keyboard=True)
+SLEEP_KEYBOARD = ReplyKeyboardMarkup([["Назад"]], resize_keyboard=True)
+MOOD_KEYBOARD = ReplyKeyboardMarkup([["Назад"]], resize_keyboard=True)
+WATER_KEYBOARD = ReplyKeyboardMarkup([["Назад"]], resize_keyboard=True)
+PILLS_KEYBOARD = ReplyKeyboardMarkup([["Назад"]], resize_keyboard=True)
+
+# это чтобы клавиатура не сьезжала
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Выберите раздел:",
+        reply_markup=START_KEYBOARD
+    )
+# главный экран в самом начале
+
+
+async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+# далее кнопки, которые мы выбираем
+
+    if text == "Зарядка":
+        await update.message.reply_text(
+            "Вы в разделе зарядки",
+            reply_markup=CHARGE_KEYBOARD
+        )
+    elif text == "Трекер сна":
+        await update.message.reply_text(
+            "Вы в трекере сна",
+            reply_markup=SLEEP_KEYBOARD
+        )
+    elif text == "Трекер настроения":
+        await update.message.reply_text(
+            "Вы в трекере настроения",
+            reply_markup=MOOD_KEYBOARD
+        )
+    elif text == "Водный баланс":
+        await update.message.reply_text(
+            "Вы в разделе водного баланса",
+            reply_markup=WATER_KEYBOARD
+        )
+    elif text == "Трекер приема таблеток":
+        await update.message.reply_text(
+            "Вы в трекере приема таблеток",
+            reply_markup=PILLS_KEYBOARD
+        )
+    elif text == "Назад":
+        await start(update, context)
+
+
+def main():
+    application = Application.builder().token("7757821288:AAFO6UALvFmOVyCD7Txdi9IXq0d6541DnL0").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_menu))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
